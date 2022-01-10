199094412bec7b0c164fcf95df477496f6789d7d
Person M <michael.person@cognitic.be>
2022-01-10 10:09:39 +0100

    Add project files.

---
+++ ExempleDelegate/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExempleDelegate;
+
+Console.WriteLine("Hello, World!");
+
+Radio maRadio = new Radio(Ecoute);
+//Console.WriteLine(maRadio.Emettre());
+maRadio.Connexion();
+//if(maRadio.Abonnement!=null)
+//{
+//    maRadio.Abonnement("Le pirate est toujours là");
+//}
+
+void Ecoute(string message)
+{
+    Console.WriteLine(message);
+}
---
+++ ExempleDelegate/Radio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempleDelegate
+{
+    public delegate void DelEmission(string message); //Déclarer le prototype de mon delegate
+    public  class Radio
+    {
+        private DelEmission Abonnement;
+        public Radio(DelEmission FonctionEcoute)
+        {
+            Abonnement += FonctionEcoute;
+        }
+
+        public void Connexion()
+        {
+            Emettre();
+        }
+        
+        private void Emettre()
+        {
+            if (Abonnement != null)
+            {
+                Abonnement("Bienvenue sur la Radio Technobel");
+            }
+           
+        }
+
+    }
+}
